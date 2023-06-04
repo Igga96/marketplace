ad34bfc8cc62d70c8269f99ea8058639ff8101ac
Igga96 <91895875+Igga96@users.noreply.github.com>
2023-06-04 03:57:25 +0300

    add new component vOrder

---
+++ src/components/v-order.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="v-order">
+      <h2>Оформить заказ</h2>
+    </div>
+  </template>
+    <script>
+  import { mapGetters } from "vuex";
+  
+  export default {
+    name: "vHeader",
+    components: {},
+    props: {},
+    computed: {
+      ...mapGetters(["BASKET"])
+    },
+    methods: {},
+    watch: {},
+  };
+  </script>    
+    <style lang="scss"></style>
